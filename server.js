89509aaaa607dffaf18824536155efcae4e64ec3
Manami Nagras <nagras.m@husky.neu.edu>
2020-12-15 09:28:02 +0900

    added files for heroku

--- server.js
+++ server.js
@@ -21,3 +21,12 @@ router.get('/api/:object', async (ctx) => {
       console.log(err)
     }
   })
+
+  const path = require("path")
+  const express = require("express")
+  const app = express()
+  app.use(express.static(path.join(__dirname, "build")))
+  app.use((req, res) => {
+  res.sendFile(path.join(__dirname,"build/index.html"));
+  });
+  app.listen(process.env.PORT || 4000)
